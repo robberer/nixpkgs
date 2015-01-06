5b0e6e0a1b130c970ef9b761884ed8e704a9db5f
Longrin Wischnewski <robberer@freakmail.de>
2015-01-06 17:07:16 +0000

    revert

--- pkgs/development/interpreters/php/5.4.nix
+++ pkgs/development/interpreters/php/5.4.nix
@@ -1,8 +1,7 @@
 { stdenv, fetchurl, composableDerivation, autoconf, automake, flex, bison
 , apacheHttpd, mysql, libxml2, readline, zlib, curl, gd, postgresql, gettext
 , openssl, pkgconfig, sqlite, config, libiconv, libjpeg, libpng, freetype
-, libxslt, libmcrypt, bzip2, icu, openldap, cyrus_sasl, libmhash, freetds
-, net_snmp }:
+, libxslt, libmcrypt, bzip2, icu, openldap, cyrus_sasl, libmhash, freetds }:
 
 let
   libmcryptOverride = libmcrypt.override { disablePosixThreads = true; };
@@ -190,11 +189,6 @@ composableDerivation.composableDerivation {} ( fixed : let inherit (fixed.fixed)
         configureFlags = ["--enable-maintainer-zts"];
       };
 
-      snmp = {
-        configureFlags = ["--with-snmp=${net_snmp}"];
-        buildInputs = [net_snmp];
-      };
-
       /*
          php is build within this derivation in order to add the xdebug lines to the php.ini.
          So both Apache and command line php both use xdebug without having to configure anything.
@@ -235,7 +229,6 @@ composableDerivation.composableDerivation {} ( fixed : let inherit (fixed.fixed)
     fpmSupport = config.php.fpm or true;
     mssqlSupport = config.php.mssql or (!stdenv.isDarwin);
     ztsSupport = config.php.zts or false;
-    snmpSupport = config.php.snmp or true;
   };
 
   configurePhase = ''
